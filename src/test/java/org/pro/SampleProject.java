1ffc59575cfbf40730da36421a942c9b2879d4f6
NandhiniJaidev <nandhinijaidev2019@gmail.com>
2022-08-24 22:28:25 +0530

    Done

---
+++ src/test/java/org/pro/SampleProject.java
@@ -0,0 +1,11 @@
+package org.pro;
+
+public class SampleProject {
+	public static void main(String[] args) 
+	{
+	int a=10, b=20, c=30;
+	int sum=a+b+c;
+	System.out.println("Sum = :"+sum);
+	}
+
+}
